de40878b10c1394c88d695a85d3b63e0aceab6b7
puran <puransapkota4@gmail.com>
2022-06-25 15:48:53 +1000

    second commit

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,42 @@ import React from 'react';
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from './MessageSender';
+import Post from './Post';
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel/>
       <MessageSender/>
+
+
+      <Post
+        profilePic="images/elon.jpeg"
+        message="hi this is my first web "
+        timestamp="hey this is puram"
+        username={"Puran"}
+        image ="images/Teacher.jpeg"
+       />
+        
+        
+      <Post
+      profilePic="images/uni.jpeg"
+      message={"hi this is my vu uni social site  "}
+      timestamp="timestamp"
+      username={"Prashant"}
+      image ="images/Teacher.jpeg"
+      />
+      <Post
+      profilePic="images/logo.jpeg"
+      message={"fuck you  "}
+      timestamp="timestamp"
+      username={"oliva"}
+      image ="images/Teacher.jpeg"
+      
+      
+      />
+    
+      
       
     </div>
   );
